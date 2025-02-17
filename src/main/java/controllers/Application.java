1e67fcdd7076d6d718328cc2e7b9de2439cc0d5f
Ole Sandum <ole@sandum.net>
2025-02-17 16:40:42 +0100

    Implementing only the core two endpoint methods

--- src/main/java/controllers/Application.java
+++ src/main/java/controllers/Application.java
@@ -1,5 +1,14 @@
 package controllers;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.util.EnumMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
@@ -7,95 +16,53 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.EnumMap;
-import java.util.List;
-import java.util.Map;
-import javax.imageio.ImageIO;
+
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import models.ShortScheme;
 import models.ShortUrlTag;
-import org.apache.commons.codec.binary.Base64;
-import play.Logger;
-import play.cache.Cached;
-import play.data.Form;
-import play.mvc.*;
-import views.html.*;
 
-public class Application extends Controller {
+@Path("/")
+public class Application {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+    @GET
+    @Path("/{scheme}/{target}-qr.png")
+    @Produces("image/png")
+    public Response getQRCodeAsPNG(@PathParam("scheme") String schemeName, 
+                                   @PathParam("target") String target,
+                                   @QueryParam("size") @DefaultValue("256") Integer size,
+                                   @QueryParam("margin") @DefaultValue("0") Integer margin,
+                                   @QueryParam("ecc") @DefaultValue("M") String ecc) throws WriterException, IOException 
+    {
+        ShortScheme scheme = ShortScheme.find("name", schemeName).firstResult();
+        if (scheme == null)
+            return Response.status(Status.BAD_REQUEST).entity("'" + schemeName + "': unrecognized URL scheme").build();
 
-    public static Result updateScheme(Long id) {
-        Form<ShortScheme> form = form(ShortScheme.class).bindFromRequest();
-
-        if (form.hasErrors())
-            return badRequest(edit.render(id, form));
-
-        form.get().update(id);
-        flash("success", "URL Scheme " + form.get().name + " has been updated");
-
-        return redirect(routes.Application.schemeList());
-    }
-
-    public static Result editScheme(Long id) {
-        Form<ShortScheme> form =
-                form(ShortScheme.class).fill(ShortScheme.find.byId(id));
-        return ok(edit.render(id, form));
-    }
-
-    public static Result createScheme() {
-        return ok(index.render("Your new application is ready."));
-    }
-
-    public static Result schemeList() {
-        List<ShortScheme> all = ShortScheme.find.orderBy().asc("name").findList();
-
-        return ok(list.render(all));
-    }
-
-    @Cached(key = "tag")
-    public static Result redirectTag(String tag) {
-        ShortUrlTag url = ShortUrlTag.find.byId(tag);
-        if (url == null)
-            return notFound(pageNotFound.render(tag));
-
-        ShortScheme scheme = url.scheme;
-        String target = url.target;
-
-        String rep = scheme.expandTarget(target);
-
-        return redirect(rep);
-    }
-
-    public static Result shortTag(String schemeName, String target) throws IOException, WriterException {
-
-        ShortUrlTag shortcut = findOrCreateShortcut(schemeName, target);
+        ShortUrlTag shortcut = ShortUrlTag.findOrCreate(scheme, target);
         String shortUrl = shortcut.getShortcutUrl();
-        byte png[] = qrCode(shortUrl, null, null, null);
-        String qrBase64 = new String(Base64.encodeBase64(png));
-    //  return ok(qrBase64).as("text/plain");
-        return ok(viewTag.render(shortcut, shortUrl, qrBase64));
+
+        byte png[] = getQRCodeAsPNG(shortUrl, size, margin, ecc);
+        return Response.ok(png).build();
     }
 
-    public static Result shortTagQR(String schemeName, String target, Integer size, Integer margin, String ecc) throws IOException, WriterException {
-        ShortUrlTag shortcut = findOrCreateShortcut(schemeName, target);
-        String shortUrl = shortcut.getShortcutUrl();
-        byte png[] = qrCode(shortUrl, size, margin, ecc);
-        Logger.info("PNG (size: " + png.length+ " bytes)");
-        return ok(png).as("image/png");
-    }
+    @GET
+    @Path("{tag}")
+    public Response redirectTag(@PathParam("tag") String tag) {
+        ShortUrlTag shortcut = ShortUrlTag.findByTag(tag);
+        if (shortcut == null)
+            return Response.status(Status.NOT_FOUND).entity("'" + tag + "': not found").build();
 
-    private static byte[] qrCode(String shortUrl, Integer size, Integer margin, String ecc) throws IOException, WriterException {
-        if (size == null)
-            size = 256;
-        if (margin == null)
-            margin = 0;
-        if (ecc == null)
-            ecc = "M";
+        URI rep = shortcut.getTargetURI();
+        return Response.seeOther(rep).build();
+   }
+
+    private byte[] getQRCodeAsPNG(String shortUrl, int size, int margin, String ecc) throws WriterException, IOException {  
 
         QRCodeWriter writer = new QRCodeWriter();
         Map<EncodeHintType, Object> hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
@@ -111,36 +78,5 @@ public class Application extends Controller {
         ByteArrayOutputStream bo = new ByteArrayOutputStream();
         ImageIO.write(bi, "PNG", bo);
         return bo.toByteArray();
-    }
-
-    public static Result putShortTag(String schemeName, String target) {
-        ShortUrlTag shortcut = findOrCreateShortcut(schemeName, target);
-        return created(shortcut.getShortcutUrl());
-    }
-
-    private static ShortUrlTag findOrCreateShortcut(String schemeName, String target) {
-        // 1) Locate the short URL scheme:
-        ShortScheme sch = ShortScheme.find.where().eq("name", schemeName).findUnique();
-        if (sch == null)
-            throw new IllegalArgumentException(schemeName + ": no such URL scheme");
-
-        // 2) Check if this target has a shortened tag already:
-        ShortUrlTag shortcut = ShortUrlTag.find.where()
-                .eq("scheme", sch)
-                .eq("target", target).findUnique();
-
-        if (shortcut == null) {
-            if (!sch.accepts(target))
-                throw new IllegalArgumentException(target + ": invalid shortcut target");
-
-            shortcut = new ShortUrlTag();
-            shortcut.scheme = sch;
-            shortcut.tag = sch.generateTag();
-            shortcut.target = target;
-            shortcut.save();
-
-            Logger.info(shortcut + ": shortcut created");
-        }
-        return shortcut;
     }
 }
