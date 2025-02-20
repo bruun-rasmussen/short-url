f3ae5f8d2f2a28e719328e68f483c51a81c9d544
Ole Sandum <ole@sandum.net>
2025-02-20 12:43:30 +0100

    Test case added

---
+++ src/test/java/controllers/ApplicationTest.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+@TestHTTPEndpoint(Application.class)
+public class ApplicationTest {
+
+    @Test
+    public void getKnownShortcut() {
+        given()
+                .redirects().follow(false)
+                .get("/{tag}", "cVsWU")
+                .then()
+                .statusCode(303);
+    }
+
+    @Test
+    public void getKnownShortcutInfo() {
+        given()
+                .redirects().follow(false)
+                .get("/{tag}?info=true", "cVsWU")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void getByIdNotFound() {
+        given()
+                .redirects().follow(false)
+                .get("/{tag}", 987654321)
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void getQRCodeAsPNG() {
+        given()
+                .redirects().follow(false)
+                .get("/{scheme}/{target}-qr.png", "lot-id", "4D48C8650442")
+                .then()
+                .statusCode(200)
+                .contentType("image/png");
+    }   
+}
